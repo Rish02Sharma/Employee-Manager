bc7b4008ed3c4f8765b069f3ab4552a15b4249d6
Rishabh Sharma <15ucc030@lnmiit.ac.in>
2018-07-06 13:36:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import Router from './Router';
 class App extends Component {
   componentWillMount() {
     const config = {
-      apiKey: 'AIzaSyBR4FTGiOcTZ5OBNi6thE0KhCDR3PaOtQM',
-      authDomain: 'manager-2328b.firebaseapp.com',
-      databaseURL: 'https://manager-2328b.firebaseio.com',
-      storageBucket: 'manager-2328b.appspot.com',
-      messagingSenderId: '193075244299'
+      
     };
 
     firebase.initializeApp(config);
